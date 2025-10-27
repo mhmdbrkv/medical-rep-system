fcfb194013ef0c57fe1da6863d8dfd5cb457569f
mohamed baraka <barakamohamed946@gmail.com>
2025-10-27 12:34:56 +0300

    add appErros.js in utils folder

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const CLIENT_URL = process.env.CLIENT_URL || "/";
 const MANAGER_EMAIL = process.env.MANAGER_EMAIL || "";
 const MANAGER_PASSWORD = process.env.MANAGER_PASSWORD || "";
 const DATABASE_URL = process.env.DATABASE_URL || "";
-const NODE_ENV = process.env.NODE_ENV || "developments";
+const NODE_ENV = process.env.NODE_ENV || "development";
 const JWT_ACCESS_SECRET_KEY = process.env.JWT_ACCESS_SECRET_KEY || "";
 const JWT_ACCESS_EXPIRE_TIME = process.env.JWT_ACCESS_EXPIRE_TIME || "";
 
