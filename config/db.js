a6d281188381cfe0cf157f724efb30206ea63526
mohamed baraka <barakamohamed946@gmail.com>
2025-12-13 12:53:04 +0200

    fix prisma on deploy

--- config/db.js
+++ config/db.js
@@ -5,6 +5,10 @@ import { DATABASE_URL } from "./index.js";
 const connectionString = `${DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export { prisma };
