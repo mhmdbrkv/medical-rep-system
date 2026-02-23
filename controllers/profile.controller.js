3c99588d5657cf6bec94360194fb1cc818e665e1
mohamed baraka <barakamohamed946@gmail.com>
2026-02-23 13:04:31 +0200

    add upload and remove profile image

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,5 +1,11 @@
 import { prisma } from "../config/db.js";
 
+import { ApiError } from "../utils/apiError.js";
+import {
+  removeImageFromCloudinary,
+  uploadImageToCloudinary,
+} from "../utils/cloudinary.js";
+
 // Profile Controllers
 const getProfile = async (req, res) => {
   const data = await prisma.user.findUnique({
@@ -31,4 +37,81 @@ const updateProfile = async (req, res) => {
   });
 };
 
-export { getProfile, updateProfile };
+const uploadProfileImage = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      return next(new ApiError("No file uploaded", 400));
+    }
+
+    if (
+      req.file.mimetype !== "image/jpeg" &&
+      req.file.mimetype !== "image/png"
+    ) {
+      return next(new ApiError("Only JPEG and PNG images are allowed", 400));
+    }
+
+    // upload image to cloudinary
+
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.id },
+      select: { profileImage: true },
+    });
+
+    if (user?.profileImage && user.profileImage?.public_id) {
+      await removeImageFromCloudinary(user.profileImage?.public_id);
+    }
+
+    const result = await uploadImageToCloudinary(req.file.buffer, {
+      public_id: `profile_${req.user.id}_${Date.now()}`,
+      folder: "folder-files/profiles",
+    });
+
+    let imageData =
+      { public_id: result.public_id, url: result.secure_url } || {};
+
+    const data = await prisma.user.update({
+      where: { id: req.user.id },
+      data: {
+        profileImage: imageData,
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      message: "Data updated successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    next(new ApiError(`Upload Profile Image Error: ${error}`, 500));
+  }
+};
+
+const removeProfileImage = async (req, res, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.id },
+      select: { profileImage: true },
+    });
+
+    if (user?.profileImage && user.profileImage?.public_id) {
+      await removeImageFromCloudinary(user.profileImage?.public_id);
+    }
+
+    const data = await prisma.user.update({
+      where: { id: req.user.id },
+      data: {
+        profileImage: null,
+      },
+    });
+    res.status(200).json({
+      status: "success",
+      message: "Data updated successfully",
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    next(new ApiError(`Remove Profile Image Error: ${error}`, 500));
+  }
+};
+
+export { getProfile, updateProfile, uploadProfileImage, removeProfileImage };
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -4,7 +4,10 @@ const router = express.Router();
 import {
   getProfile,
   updateProfile,
+  uploadProfileImage,
+  removeProfileImage,
 } from "../controllers/profile.controller.js";
+import { imageUpload } from "../utils/multer.js";
 import { guard } from "../middlewares/auth.middleware.js";
 
 router.use(guard);
@@ -12,5 +15,7 @@ router.use(guard);
 // Profile Routes
 router.get("/", getProfile);
 router.patch("/", updateProfile);
+router.post("/profile-image", imageUpload, uploadProfileImage);
+router.delete("/profile-image", removeProfileImage);
 
 export default router;
